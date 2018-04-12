0f75d2f2e8906e08f2894aeb57028e36c585e8a3
Sebastian <seebzei@gmail.com>
2018-04-12 22:08:15 +0200

    Add TextureManager

---
+++ EksamenBase/EksamenBase/TextureManager.h
@@ -0,0 +1,10 @@
+#pragma once
+class TextureManager
+{
+public:
+	TextureManager();
+	~TextureManager();
+
+	// Load surfaces into textures and keep the pointer to the textures in a hashmap, can look it up with imagepath w
+};
+
